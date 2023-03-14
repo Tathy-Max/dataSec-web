3c78feb35392665f5bb0fde1c737bf1d3c77662d
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2023-03-14 12:37:53 -0400

    tech v1

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,8 +1,97 @@
 import React from 'react';
 import './tech.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Tech = () => {
-	return <div></div>;
+	return (
+		<section id="tech">
+			<h5>We've been using</h5>
+			<h2>Technologies and Methodologies applied</h2>
+
+			<div className="container tech__container">
+				<div className="tech__technology">
+					<h3>Technologies</h3>
+					<div className="tech__content">
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>Microsoft</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>Linux</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>Cobit</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>Sophos</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>FortiNET</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+					</div>
+				</div>
+				<div className="tech__methodology">
+					<h3>Methodologies</h3>
+					<div className="tech__content">
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>ITIL</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>NIST CSF</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>AWS</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>Tenable Nessus</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+						<article className="tech__details">
+							<BsPatchCheckFill className="tech__details-icon" />
+							<div>
+								<h4>Graylog</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Tech;

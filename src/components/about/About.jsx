6066f3594e500ca92cbc5c52d7a2343aa40539a5
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2023-03-13 21:04:54 -0400

    navbar + about v1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me2.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
-function About() {
-	return <div>About</div>;
-}
+const About = () => {
+	return (
+		<section id="about">
+			<h5>Get to know</h5>
+			<h2>Sobre nos</h2>
+			<div className="container about__container">
+				<div className="about__me">
+					<div className="about__me-image">
+						<img src={ME} alt="About Logo" />
+					</div>
+				</div>
+
+				<div className="about__content">
+					<div className="about__cards">
+						<article className="about__card">
+							<FaAward className="about__icon" />
+							<h5>Experience</h5>
+							<small>15+ Years Working</small>
+						</article>
+
+						<article className="about__card">
+							<FiUsers className="about__icon" />
+							<h5>Clientes</h5>
+							<small>200+ Worldwide</small>
+						</article>
+
+						<article className="about__card">
+							<VscFolderLibrary className="about__icon" />
+							<h5>Projetos</h5>
+							<small>500+ Completed</small>
+						</article>
+					</div>
+
+					<p>
+						Profissionais experientes compromissados em levar os melhores
+						serviços com valores justos e acessíveis. Acreditamos que os
+						melhores processos, tecnologia e segurança de seus dados e operações
+						são fundamentos primordiais para o negócio e portanto devem ser
+						acessíveis a todos. Vasta experiência com softwares e plataformas
+						tradicionais e Open Source.
+					</p>
+
+					<a href="#contact" className="btn btn-primary">
+						Fale Conosco
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default About;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import './navbar.css';
+import { AiFillHome } from 'react-icons/ai';
+import { HiInformationCircle } from 'react-icons/hi';
+import { MdOutlineMiscellaneousServices } from 'react-icons/md';
+import { BsFillPeopleFill } from 'react-icons/bs';
+import { IoMdMail } from 'react-icons/io';
+import { useState } from 'react';
 
-function Navbar() {
-	return <div>Navbar</div>;
-}
+const Nav = () => {
+	const [activeNav, setActiveNav] = useState('#');
+	return (
+		<nav>
+			<a
+				href="#"
+				onclick={() => setActiveNav('#')}
+				className={activeNav === '#' ? 'active' : ''}
+			>
+				<AiFillHome />
+			</a>
+			<a
+				href="#about"
+				onclick={() => setActiveNav('#about')}
+				className={activeNav === '#about' ? 'active' : ''}
+			>
+				<HiInformationCircle />
+			</a>
+			<a
+				href="#services"
+				onclick={() => setActiveNav('#services')}
+				className={activeNav === '#services' ? 'active' : ''}
+			>
+				<MdOutlineMiscellaneousServices />
+			</a>
+			<a
+				href="#testimonials"
+				onclick={() => setActiveNav('#testimonials')}
+				className={activeNav === '#testimonials' ? 'active' : ''}
+			>
+				<BsFillPeopleFill />
+			</a>
+			<a
+				href="#contact"
+				onclick={() => setActiveNav('#contact')}
+				className={activeNav === '#contact' ? 'active' : ''}
+			>
+				<IoMdMail />
+			</a>
+		</nav>
+	);
+};
 
-export default Navbar;
+export default Nav;
